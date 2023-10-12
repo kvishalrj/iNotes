24b8a00b34f5bac0eb2eab519b31b3dd8d8e0ca6
Vishal Kumar <kvishalrj2020@gmail.com>
2023-10-12 12:15:59 +0530

    first commit

---
+++ config/db.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://kvishal:PqZJdKhoIav1Y63f@vmcluster.x0iz634.mongodb.net/"
+
+conn = MongoClient(MONGO_URI)
---
+++ routes/note.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from models.note import Note
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from config.db import conn
+from fastapi.templating import Jinja2Templates
+
+note = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@note.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    docs = conn.vmdb.vmnotes.find({})
+    newDocs = []
+    for doc in docs:
+        newDocs.append({
+            "id": doc["_id"],
+            "title": doc["title"],
+            "desc": doc["desc"]
+        })
+    return templates.TemplateResponse("index.html", {"request": request, "newDocs": newDocs})
+
+
+@note.post("/")
+async def create_item(request: Request):
+    form = await request.form()
+    formDict = dict(form)
+    if formDict["title"]=="" or formDict["desc"]=="":
+        return {"success": False}
+    else: 
+        note = conn.vmdb.vmnotes.insert_one(formDict) 
+        return {"success": True}
